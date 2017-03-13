95461faba97c0cb92b0d6b20b2584786b9f2b45e
shiv <shiva.shivjamesh@gmail.com>
2017-03-13 16:08:10 +0530

    Finish toy app

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+    .
+    .
+    .
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    .
+    .
+    .
+  end
+
+  # GET /users/new
+  def new
+    .
+    .
+    .
+  end
+
+  # GET /users/1/edit
+  def edit
+    .
+    .
+    .
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    .
+    .
+    .
+  end
+
+  def destroy.
+    .
+    .
+    .
+  end
+end
